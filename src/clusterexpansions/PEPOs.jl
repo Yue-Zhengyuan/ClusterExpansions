3b52419e3fa32fdf07abcf346d0b7ff88239c006
Yue Zhengyuan <yuezy1997@icloud.com>
2026-03-13 13:54:48 +0800

    Minor refactor

--- src/clusterexpansions/PEPOs.jl
+++ src/clusterexpansions/PEPOs.jl
@@ -25,7 +25,14 @@ function get_PEPO(T, pspace, PEPO, spaces)
     highest = [maximum([i[dir] for i in keys(PEPO)]) for dir in 1:4]
     highest_loop = [minimum([i[dir] for i in keys(PEPO)]) for dir in 1:4]
     conjugated = Bool[0, 0, 1, 1]
-    O = zeros(T, SumSpace(pspace) ⊗ SumSpace(pspace)', ⊗([conj ? (get_sum_space(h, hloop, spaces))' : get_sum_space(h, hloop, spaces) for (conj, h, hloop) in zip(conjugated, highest, highest_loop)]...))
+    Oout = SumSpace(pspace) ⊗ SumSpace(pspace)'
+    Oin = ⊗(
+        [
+            conj ? (get_sum_space(h, hloop, spaces))' : get_sum_space(h, hloop, spaces)
+                for (conj, h, hloop) in zip(conjugated, highest, highest_loop)
+        ]...
+    )
+    O = zeros(T, Oout ← Oin)
     for (key, tens) in PEPO
         key = [i < 0 ? h - i + 1 : i + 1 for (i, h) in zip(key, highest)]
         # places = [get_location_PEPO(ind, highest[dir], spaces) for (dir,ind) = enumerate(key)]
